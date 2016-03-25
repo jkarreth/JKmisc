a0bd0054019864151d66618caade85e83a81375f
Johannes Karreth <jskarreth@gmail.com>
2016-03-25 16:18:32 -0400

    Updated examples

--- robclSE.demo.R
+++ robclSE.demo.R
@@ -1,111 +1,109 @@
 #######################################################
 ### Illustrated code to obtain robust and clustered ###
-###  standard errors after OLS in R                 ###
+###  standard errors from OLS in R                  ###
 ### Johannes Karreth                                ###
-### 30/03/2015                                      ###
+### 03/25/2016                                      ###
 #######################################################
 
-# NOTE: Robust and clustered standard errors are not the same, 
-# and "correct" for very different structures in the data. 
+# NOTE: This code is provided for teaching purposes to students in my seminar
+# RPOS/RPAD 417, Quantitative Research Methods, at the University at Albany.
+# Please refer to the accompanying course materials for this course for
+# more information at <http://www.jkarreth.net/rpos517.html>.
 
 # Robust standard errors were proposed by White (1980) in order to 
 # calculate standard errors when the errors are heteroskedastic but 
-# still independent (for more, see Fox pp. 275-277).
+# still independent (for more, see Fox pp. 275-277 (2nd ed.)).
 
-# Clustered SEs are sometimes used when observations come from "clusters".
-# If you want to assume that observations coming from the same cluster are not 
+# Clustered SEs correct for the clustering of observations in groups.
+# If you don't want to assume that observations coming from the same cluster are still
 # independent (i.e. they have a different error structure than observations
 # from a different cluster), clustered SEs adjust SE estimates for this data
 # structure. 
 
-# But note that neither robust nor clustered SEs adjust your coefficinet estimates.
+# But note that neither robust nor clustered SEs adjust your coefficient estimates.
 # They also do not give you much useful information about the quality of your model
-# and the actual structure of your data. For more on this, see King and Roberts (2014).
+# and the actual structure of your data. 
 
 # Functions for this code:
 # robSE <github>
 # clSE <github>
 
-### Load example data from John Fox's book
+### 1. Robust standard errors
 
-slid.dat <- read.csv("http://www.jkarreth.net/files/car_SLID.csv")
+# Load example data from John Fox's book
+# See ?car::SLID for more information on these data
 
-# Before advancing, make sure to check that there are no missing observations
-# for outcomes, covariates, and cluster variables
-summary(slid.dat)
+slid_dat <- read.csv("http://www.jkarreth.net/files/car_SLID.csv")
+summary(slid_dat)
 
-### Fit OLS as usual
+# Fit OLS as usual: outcome are individuals' wages
 
-mod <- lm(wages ~ education + age + sex, data = slid.dat)
+slid_mod <- lm(wages ~ education + age + sex + language, data = slid_dat)
+summary(slid_mod)
 
-### Read in clSE and robSE functions from my GitHub repository "JKmisc".
-### To do this, use the source_url function from the devtools package
+# Load robSE function from my github repository
 
-library(devtools)
+devtools::source_url("https://raw.githubusercontent.com/jkarreth/JKmisc/master/robSE.R")
 
-# source("/Users/johanneskarreth/R/JKmisc/clSE.R")
-source_url("https://raw.githubusercontent.com/jkarreth/JKmisc/master/clSE.R")
-# source("/Users/johanneskarreth/R/Source/JKmisc/robSE.R")
-source_url("https://raw.githubusercontent.com/jkarreth/JKmisc/master/robSE.R")
+# Create a vector with robust SEs 
 
-### Robust SEs
+slid_mod.rse <- robSE(mod = slid_mod)
 
-# Create a vector with robust SEs and the associated p-values, if needed
+# and the associated p-values, if needed
 
-mod.rse <- robSE(mod)
-mod.rse.pval <- (1 - pnorm(abs(coef(mod) / mod.rse))) * 2
+slid_mod.rse.pval <- (1 - pnorm(abs(coef(slid_mod) / slid_mod.rse))) * 2
 
-### Clustered SEs
-
-# If there are missing observations and you want to use clustered standard errors, 
-# create a new dataset where all missing observations on the relevant variables are dropped.
-# If you want to estimate a model of "wages" with the following predictors:
-# "education", "age", "sex"
-# and you assume that your errors are not independent across subjects' languages, 
-# then your cluster variable is "language" and you would use the following command:
-slid.dat.noNa <- na.omit(slid.dat[, c("wages", "education", "age", "sex", "language")])
-summary(slid.dat.noNa)
-
-# Fit the model on the data without missing observations
-mod.noNa <- lm(wages ~ education + age + sex, data = slid.dat.noNa)
-
-# Create a vector with clustered SEs and the associated p-values, if needed
-
-mod.cse <- clSE(data = slid.dat.noNa, model = mod.noNa, cluster = "language")
-mod.cse.pval <- (1 - pnorm(abs(coef(mod) / mod.cse))) * 2
-
-
-### Create regression table for Word or LaTeX
-# Note that in this table, the N for each model differs because the model with 
-# clustered SEs had to drop observations for which we don't have info on the 
-# clustering variable ("language"). If the models were fit to the exactly same sample,
-# the coefficient estimates would be identical. Only the SEs would differ.
+# Combine conventional and robust SEs in one regression table, using screenreg()
 
 library(texreg)
+screenreg(list(slid_mod, slid_mod), 
+          override.se = list(coef(summary(slid_mod))[, 2], slid_mod.rse),
+          override.pval = list(coef(summary(slid_mod))[, 4], slid_mod.rse.pval),
+          custom.model.names = c("Conventional SEs", "Robust SEs"),
+          digits = 3)
 
-screenreg(list(mod, mod.noNa), 
-            override.se = list(mod.rse, mod.cse),
-            override.pval = list(mod.rse.pval, mod.cse.pval),
-            custom.model.names = c("Robust SEs", "Clustered SEs"),
-            digits = 3
-  )
 
-htmlreg(list(mod, mod.noNa), 
-          override.se = list(mod.rse, mod.cse),
-          override.pval = list(mod.rse.pval, mod.cse.pval),
-          custom.model.names = c("Robust SEs", "Clustered SEs"),
-          digits = 3,
-          caption = "OLS estimates of wages",
-          caption.above = TRUE,
-          file = "wagesreg.doc"
-)
+### 2. Clustered standard errors
 
-texreg(list(mod, mod.noNa), 
-        override.se = list(mod.rse, mod.cse),
-        override.pval = list(mod.rse.pval, mod.cse.pval),
-        custom.model.names = c("Robust SEs", "Clustered SEs"),
-        digits = 3,
-        caption = "OLS estimates of wages",
-        caption.above = TRUE,
-        file = "wagesreg.tex"
-)
+# This function requires complete data (no missing observations) on all variables
+# in the model PLUS the clustering variable. It will return an error if there are 
+# missing values on the clustering variable. 
+
+devtools::source_url("https://raw.githubusercontent.com/jkarreth/JKmisc/master/clSE.R")
+
+# Load example data from Alvarez, Garrett, and Lange 1991 (APSR 85 (2), 539-556)
+# Online at Alvarez's website <http://people.hss.caltech.edu/~rma/replication.html>
+# or download a copy from my website <http://www.jkarreth.net/files/growth.dta>
+# Unit of observation: country-year (i.e. multiple obs. per country)
+
+agl_dat <- rio::import("http://www.hss.caltech.edu/~rma/agl_verified.dta")
+summary(agl_dat)
+
+# Rescale some variables
+agl_dat$opengdp2 <- agl_dat$opengdp / 100
+agl_dat$openex2 <- agl_dat$openex / 100
+agl_dat$openimp2 <- agl_dat$openimp / 100
+agl_dat$leftc2 <- agl_dat$leftc / 100
+
+# Fit OLS as usual: outcome is economic growth
+
+agl_mod <- lm(growth ~ opengdp2 + openex2 + openimp2 + leftc2 + central, data = agl_dat)
+
+# Create a vector with SEs clustered on countries
+# clSE() takes three arguments: the dataframe (df), the lm() object (mod),
+# and the name of the clustering variable (cluster)
+
+agl_mod.cse <- clSE(df = agl_dat, mod = agl_mod, cluster = "country")
+
+# associated p-values, if needed
+
+agl_mod.cse.pval <- (1 - pnorm(abs(coef(agl_mod) / agl_mod.cse))) * 2
+
+# Combine conventional and robust SEs in one regression table, using screenreg()
+
+library(texreg)
+screenreg(list(agl_mod, agl_mod), 
+          override.se = list(coef(summary(agl_mod))[, 2], agl_mod.cse),
+          override.pval = list(coef(summary(agl_mod))[, 4], agl_mod.cse.pval),
+          custom.model.names = c("Conventional SEs", "Clustered SEs"),
+          digits = 3)
