027278d1023a93d0a55a86c98bf34780685a9d3e
Johannes Karreth <jskarreth@gmail.com>
2016-07-06 14:29:16 -0400

    allows stanfit objects as direct input

--- mcmctab.R
+++ mcmctab.R
@@ -7,14 +7,17 @@
 # the % of posterior draws below/above 0 from MCMC output from 
 # R2jags, rjags, R2WinBUGS, R2OpenBUGS, and MCMCpack
 
-# Arguments: 
-# sims: output from R2jags, rjags, R2WinBUGS, R2OpenBUGS, MCMCpack
-# This also works with rstan objects, but you need to pass them on as 
-# MCMC lists. If "fit" is the rstan object, pass it to this function as
-# mcmctab(sims = rstan::As.mcmc.list(fit))
+# Depends on packages: coda, rstan (if working with rstan objects)
 
-mcmctab <- function(sims, ci = 0.95) 
-{
+# Arguments: 
+# sims: output from R2jags, rjags, R2WinBUGS, R2OpenBUGS, MCMCpack, rstan
+# ci: desired credible interval, default: 0.95
+# digits: desired number of digits in the table, default: 2
+
+mcmctab <- function(sims, ci = 0.95, digits = 2){
+  
+  require(coda)	
+  
   if(class(sims) == "jags" | class(sims) == "rjags"){
     sims <- as.matrix(as.mcmc(sims))
   }
@@ -27,14 +30,18 @@ mcmctab <- function(sims, ci = 0.95)
   if(class(sims) == "mcmc.list"){
     sims <- as.matrix(sims)
   }      
+  if(class(sims) == "stanfit"){
+    stan_sims <- rstan::As.mcmc.list(sims)
+    sims <- as.matrix(stan_sims)
+  }      
   
   dat <- t(sims)
-    mcmctab <- apply(dat, 1, 
-    	function(x) c(Mean = round(mean(x), digits = 3), # Posterior mean
-    		SD = round(sd(x), digits = 3), # Posterior SD
-    		Lower = as.numeric(round(quantile(x, probs = c((1 - ci) / 2)), digits = 3)), # Lower CI of posterior
-    		Upper = as.numeric(round(quantile(x, probs = c((1 + ci) / 2)), digits = 3)), # Upper CI of posterior
-    		Pr = round(ifelse(mean(x) > 0, length(x[x > 0]) / length(x), length(x[x < 0]) / length(x)), digits = 3) # Probability of posterior >/< 0
-    		))
-    return(t(mcmctab))
+  mcmctab <- apply(dat, 1, 
+                   function(x) c(Mean = round(mean(x), digits = digits), # Posterior mean
+                                 SD = round(sd(x), digits = 3), # Posterior SD
+                                 Lower = as.numeric(round(quantile(x, probs = c((1 - ci) / 2)), digits = digits)), # Lower CI of posterior
+                                 Upper = as.numeric(round(quantile(x, probs = c((1 + ci) / 2)), digits = digits)), # Upper CI of posterior
+                                 Pr = round(ifelse(mean(x) > 0, length(x[x > 0]) / length(x), length(x[x < 0]) / length(x)), digits = digits) # Probability of posterior >/< 0
+                   ))
+  return(t(mcmctab))
 }
