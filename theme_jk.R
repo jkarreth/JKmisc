c9b2cde26dbf9927ca62351dec01c11eb1557e9d
Johannes Karreth <jkarreth@ursinus.edu>
2016-12-20 14:52:31 -0500

    Adjusted font color after ggplot2 update

--- theme_jk.R
+++ theme_jk.R
@@ -11,6 +11,7 @@ theme_jk <- function(base_size = 11, base_family = "Helvetica")
       panel.border = element_rect(fill = NA, color = "black", size = 0.75),
       plot.title = element_text(size = rel(1.2), margin = margin(b = base_size/2 * 1.2), face = "bold"),
       panel.grid.major = element_line(colour = "grey85", size = 0.4, linetype = 3), 
-      panel.grid.minor = element_line(colour = "grey90", size = 0.2, linetype = 3)
+      panel.grid.minor = element_line(colour = "grey90", size = 0.2, linetype = 3),
+      axis.text = element_text(color = "black")
     )
 }
