f462d75930b5f96d9384a059006c1bc92dff5cd2
Johannes Karreth <jkarreth@ursinus.edu>
2021-05-24 15:05:20 -0400

    update panel title layout

--- theme_jk.R
+++ theme_jk.R
@@ -2,7 +2,7 @@ theme_jk <- function(base_size = 11, base_family = "Helvetica")
 {
     theme_bw(base_size = base_size, base_family = base_family) %+replace%
         theme(
-            strip.background = element_rect(fill = NA, color = "black", size = 0),
+            strip.background = element_rect(fill = NA, color = NA, size = 0),
             strip.text.x = element_text(size = rel(1.25), margin = margin(t = base_size/2 * 0.6, b = base_size/2 * 0.6), face = "bold"),
             panel.border = element_rect(fill = NA, color = "black", size = 0.75),
             plot.title = element_text(size = rel(1.2), margin = margin(b = base_size/2 * 1.2), face = "bold", hjust = 0.5),
